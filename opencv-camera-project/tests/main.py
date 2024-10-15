81977a15dbae03a9f20e1ec12267d0711e3f192d
Adeyemi Ekundayo <ekundayo@udel.edu>
2024-10-15 16:46:00 -0400

    Main updated.

--- opencv-camera-project/tests/main.py
+++ opencv-camera-project/tests/main.py
@@ -5,8 +5,7 @@ import sys
 import os
 
 # Add the src directory to the Python path to allow importing modules from it.
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'srcs')))
 from components.camera.camera_handler import CameraHandler
 from utils.image_processing import apply_grayscale
 
